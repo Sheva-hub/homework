9cccf05cf75221144ff9d555670289b59216ff30
Sheva-hub <74480744+Sheva-hub@users.noreply.github.com>
2021-07-12 18:50:02 +0900

    Create Calc.vue

---
+++ Calc.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <input v-model.number="operand1" type="number" placeholder="operand1" />
+      <input v-model.number="operand2" type="number" placeholder="operand2" />
+      ={{ sum }}
+    </div>
+    <div>
+      <button @click="sum = operand1 + operand2">+</button>
+      <button @click="sum = operand1 - operand2">-</button>
+      <button @click="sum = operand1 * operand2">*</button>
+      <button @click="sum = operand1 / operand2">/</button>
+      <button @click="sum = operand1 ** operand2">Возведение в степень</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calc",
+  data: () => ({
+    operand1: 0,
+    operand2: 0,
+    sum: 0,
+  }),
+};
+</script>
+
+<style>
+</style>
